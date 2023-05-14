2824364ec89b5b09d5fd5dd0cdc812f2c29ec781
NikitaDobry27 <bazhenov.nikita27@gmail.com>
2023-05-14 13:51:21 +0200

    Removed files

--- python_bot/__init__.py
+++
@@ -1,9 +0,0 @@
-from .notebook import NoteBook, UserInterface
-from .addressbook import AddressBook
-from .file_sorter import sort_files
-from .command_handlers import function
-from .main import main
-from abstract_classes import _Field, BaseRecordStorage
-
-
-__all__ = ["main", "function", "AddressBook", "NoteBook", "UserInterface", "_Field", "BaseRecordStorage", 'sort_files']
